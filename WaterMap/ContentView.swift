11ad652bcc2e81f4c7cc0fc40bdd4be4cd2407ee
Tsungyu Yu <ytyubox@gmail.com>
2019-12-06 20:58:57 +0800

    🦑 project init

---
+++ WaterMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterMap
+//
+//  Created by 游宗諭 on 2019/12/6.
+//  Copyright © 2019 游宗諭. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
